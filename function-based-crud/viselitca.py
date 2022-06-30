82658606654378f33fbc13f3a6c031c2c6300237
Edilhan <edylhan@gmail.com>
2022-06-30 14:56:38 +0600

    viselitca2

--- function-based-crud/viselitca.py
+++ function-based-crud/viselitca.py
@@ -1,6 +1,6 @@
 import random
-slova=['kniga','slovo','korabl']
-word=slova.randomchoice()
+slova=('kniga','slovo','korabl')
+word=random.choice(slova)
 word1=len(word)*'*'
 popytok = len(word)
 t=0
@@ -9,9 +9,18 @@ while t == 0:
     print('popytok: ',popytok)
     bukva = input('Введи букву на латинице: ')
     if bukva in word:
-        ind_bukvy= word.find(bukva) # достаем индекс найденной буквы
-        i1 = ind_bukvy+1
-        word1 = word1[:ind_bukvy]+bukva+word1[i1:-1] # заменяем * найденной буквой
+        ww=''
+        for i in range(len(word)):
+            if bukva == word[i]:
+                ww = ww+bukva
+            else:
+                ww = ww + word1[i]
+        word1 = ww
+
+            
+        # ind_bukvy= word.find(bukva) # достаем индекс найденной буквы
+        # i1 = ind_bukvy+1
+        # word1 = word1[:ind_bukvy]+bukva+word1[i1:-1] # заменяем * найденной буквой
         if word1==word:
             print('Вы выйграли')
             break
