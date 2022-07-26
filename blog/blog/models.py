2a5fb8a6784b48967bf2effcc7819d9535b87f6d
Edilhan <edylhan@gmail.com>
2022-07-26 19:30:02 +0600

    blog

---
+++ blog/blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE) # получаем все посты юзера
+
+    def __str__(self): #изменяем то как будет отображатся посты в админке
+        return f"{self.user.username} -> {self.title}"
+
+class Comment(models.Model):
+    body = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+
---
+++ blog/blog/views.py
@@ -0,0 +1,10 @@
+from .serializers import PostSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Post
+
+@api_view(['GET'])
+def posts_list(request):
+    queryset = Post.objects.all()
+    serializer = PostSerializer(queryset, many=True)
+    return Response(serializer.data)
