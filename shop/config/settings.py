fbedda24c0b170ab0fa108db22d3444afae7d88f
Edilhan <edylhan@gmail.com>
2022-08-05 00:18:58 +0600

    shop_new

--- shop/config/settings.py
+++ shop/config/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,9 +80,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'py21shop',
-        'PASSWORD': '9722',
-        'USER': 'edil',
+        'NAME': config('DB_NAME'),
+        'PASSWORD': config("DB_PASSWORD"),
+        'USER': config('DB_USER'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -140,7 +141,7 @@ REST_FRAMEWORK = {
 
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
@@ -153,3 +154,12 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_HOST = 'smtp.gmail.com'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS   = True
+ACTIVATE_USERS_EMAIL = True
+EMAIL_USE_SSL = False
--- shop/shop/views.py
+++ shop/shop/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import mixins
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Product, Category, Comment, Like
+from .models import Product, Category, Comment, Like, Rating
 from .serializers import ProductSerializer, CategorySerializer, CommentSerializer
 from .permissions import IsAuthor
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
 
 class CategoryViewSet(mixins.CreateModelMixin, 
                     mixins.DestroyModelMixin, 
@@ -19,6 +25,7 @@ class CategoryViewSet(mixins.CreateModelMixin,
                     GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class CommentViewSet(mixins.CreateModelMixin,
                     mixins.UpdateModelMixin,
@@ -26,10 +33,11 @@ class CommentViewSet(mixins.CreateModelMixin,
                     GenericViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAuthor]
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
-        context['request'] = self.request
+        context["request"] = self.request
         return context
 
 @api_view(["GET"])
@@ -41,4 +49,25 @@ def toggle_like(request, p_id):
         Like.objects.filter(user=user, product=product).delete()
     else:
         Like.objects.create(user=user, product=product)
-    return Response('Like toggled', 200)
+    return Response("Like toggled", 200)
+
+@api_view(["POST"])
+def add_rating(request, p_id):
+    user = request.user
+    product = get_object_or_404(Product, id=p_id)
+    value = request.POST.get("value")
+
+    if not user.is_authenticated:
+        raise ValueError("authentication credentials are not provided")
+
+    if not value:
+        raise ValueError("value is required")
+    
+    if Rating.objects.filter(user=user, product=product).exists():
+        rating = Rating.objects.get(user=user, product=product)
+        rating.value = value
+        rating.save()
+    else:
+        Rating.objects.create(user=user, product=product, value=value)
+    
+    return Response("rating created", 201)
